4ab99484f1a9f7fd4de8da34a174f46738f38fea
Sidney Andrews <sidandrews@microsoft.com>
2024-10-16 16:59:28 -0400

    Update replace.java
    Signed-off-by: Sidney Andrews <sidandrews@microsoft.com>

--- .github/workflows/replace.java
+++ .github/workflows/replace.java
@@ -1,6 +1,3 @@
-    @Bean
-    public CosmosClientBuilder getCosmosClientBuilder() {            
         return new CosmosClientBuilder()
             .endpoint("<azure-cosmos-db-nosql-endpoint>")
             .key("<azure-cosmos-db-nosql-key>");
-    }
